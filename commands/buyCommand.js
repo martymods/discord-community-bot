26aa751057e26f4851893fcf22b72bbf52010db0
martymods <50667313+martymods@users.noreply.github.com>
2025-04-14 03:15:09 -0400

    Update buyCommand.js

--- commands/buyCommand.js
+++ commands/buyCommand.js
@@ -1,10 +1,16 @@
 module.exports = {
     name: 'buy',
     execute(message) {
-      message.channel.send(`Ready to level up?
-  Choose your payment method:
-  
-  💳 Stripe → https://your-stripe-checkout-link.com  
-  💰 PayPal → https://your-paypal-checkout-link.com`);
+        message.channel.send(`Ready to level up?
+            Choose your payment method:
+            
+            💳 Hustler → https://buy.stripe.com/test_cN2eY3f2C9yef9S4gg
+            💳 Grinder → https://buy.stripe.com/test_28o7vB3jU11I8Lu145
+            💳 Plug → https://buy.stripe.com/test_4gw3fl07IdOu1j2fZ0
+            💳 Boss → https://buy.stripe.com/test_dR63fl4nYh0G7HqfZ1
+            💳 Don → https://buy.stripe.com/test_28o9DJ6w625MbXG6os
+            💳 Legend → https://buy.stripe.com/test_aEU5nt1bM7q65zi6ot
+            `);
+            
     }
   }

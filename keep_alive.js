cfcb6e9afb3552eb5c885a4c3eba65febebf06ed
martymods <50667313+martymods@users.noreply.github.com>
2025-04-14 02:29:23 -0400

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => res.send('Bot is alive!'));
+
 app.listen(3000, () => {
   console.log('Keep-alive server is running.');
 });
-require('./index');

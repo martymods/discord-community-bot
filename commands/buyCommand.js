42e2205a45f2484d8b423ad1001c4f68ad8a9a47
martymods <50667313+martymods@users.noreply.github.com>
2025-04-14 01:59:38 -0400

    Update buyCommand.js

--- commands/buyCommand.js
+++ commands/buyCommand.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module.exports = {
     name: 'buy',
     execute(message) {
@@ -9,16 +9,3 @@ module.exports = {
   💰 PayPal → https://your-paypal-checkout-link.com`);
     }
   }
-=======
-module.exports = {
-    name: 'buy',
-    execute(message) {
-      message.channel.send(`Ready to level up?
-  Choose your payment method:
-  
-  💳 Stripe → https://your-stripe-checkout-link.com  
-  💰 PayPal → https://your-paypal-checkout-link.com`);
-    }
-  }
->>>>>>> e7ad81546eaf32e3ff3aab470666bc16b1cddfb8
-  

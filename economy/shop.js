765e5c819ec1547fb82c81421a529c038b56859d
martymods <50667313+martymods@users.noreply.github.com>
2025-04-14 11:53:42 -0400

    Add files via upload

---
+++ economy/shop.js
@@ -0,0 +1,8 @@
+module.exports = {
+    shopItems: {
+      gem: { name: '💎 Gem', price: 500 },
+      medal: { name: '🎖️ Medal', price: 300 },
+      dice: { name: '🎲 Dice', price: 200 },
+    }
+  };
+  

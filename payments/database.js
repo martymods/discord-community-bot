fa64099dcb74e09bc034d47f3d5ae3ce6f775cd6
martymods <50667313+martymods@users.noreply.github.com>
2025-04-14 01:58:15 -0400

    Update database.js

--- payments/database.js
+++ payments/database.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 
 const purchaseSchema = new mongoose.Schema({
@@ -12,18 +12,3 @@ const purchaseSchema = new mongoose.Schema({
 const Purchase = mongoose.model('Purchase', purchaseSchema);
 
 module.exports = { Purchase };
-=======
-const mongoose = require('mongoose');
-
-const purchaseSchema = new mongoose.Schema({
-  userId: String,
-  username: String,
-  tier: String,
-  paymentMethod: String,
-  date: { type: Date, default: Date.now }
-});
-
-const Purchase = mongoose.model('Purchase', purchaseSchema);
-
-module.exports = { Purchase };
->>>>>>> e7ad81546eaf32e3ff3aab470666bc16b1cddfb8

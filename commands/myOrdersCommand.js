ac552b94c642757eba03202312106753acdab3bd
martymods <50667313+martymods@users.noreply.github.com>
2025-04-14 01:59:47 -0400

    Update myOrdersCommand.js

--- commands/myOrdersCommand.js
+++ commands/myOrdersCommand.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const { Purchase } = require('../payments/database');
 
 module.exports = {
@@ -13,19 +13,3 @@ module.exports = {
     message.reply(`Your purchases:\n${list}`);
   }
 }
-=======
-const { Purchase } = require('../payments/database');
-
-module.exports = {
-  name: 'myorders',
-  async execute(message) {
-    const purchases = await Purchase.find({ userId: message.author.id });
-
-    if (!purchases.length) return message.reply("You don't have any purchases yet!");
-
-    let list = purchases.map(x => `${x.tier} via ${x.paymentMethod} on ${x.date.toDateString()}`).join('\n');
-
-    message.reply(`Your purchases:\n${list}`);
-  }
-}
->>>>>>> e7ad81546eaf32e3ff3aab470666bc16b1cddfb8

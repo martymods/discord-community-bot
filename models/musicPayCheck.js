11eba65a1bb9a55bc647d90020f8864f0831b172
martymods <50667313+martymods@users.noreply.github.com>
2025-04-16 05:04:37 -0400

    Delete models/musicPayCheck.js

--- models/musicPayCheck.js
+++
@@ -1,8 +0,0 @@
-const MusicOrders = require('./models/MusicOrders'); // make sure this is CJS too
-
-async function hasPaidForSubmission(userId, guildId) {
-  const order = await MusicOrders.findOne({ userId, guildId });
-  return !!order;
-}
-
-module.exports = { hasPaidForSubmission };

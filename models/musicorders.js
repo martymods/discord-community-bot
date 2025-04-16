918a952da0be656637c879b9afbc2d848be11451
martymods <50667313+martymods@users.noreply.github.com>
2025-04-16 06:25:36 -0400

    Update musicorders.js

--- models/musicorders.js
+++ models/musicorders.js
@@ -4,6 +4,7 @@ const MusicOrderSchema = new mongoose.Schema({
   userId: String,
   guildId: String,
   paid: { type: Boolean, default: false },
+  submitted: { type: Boolean, default: false }, // ✅ Add this line
   createdAt: { type: Date, default: Date.now }
 });
 

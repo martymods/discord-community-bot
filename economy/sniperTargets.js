efd3a1dec13ff46cff029d2af81875b7a6026cb9
martymods <50667313+martymods@users.noreply.github.com>
2025-04-16 13:39:37 -0400

    Update sniperTargets.js

--- economy/sniperTargets.js
+++ economy/sniperTargets.js
@@ -22,16 +22,15 @@ function getAllSnipers() {
 }
 
 function getSniperRotation(count = 3) {
-    const shuffled = [...sniperPool].sort(() => Math.random() - 0.5);
-    return shuffled.slice(0, count);
-  }
-  
-  module.exports = {
-    addTrackedTicker,
-    removeTrackedTicker,
-    getTrackedTickers,
-    getAllSnipers,
-    getSniperRotation
-  };
-  
-  
+  const pool = getTrackedTickers();
+  const shuffled = [...pool].sort(() => Math.random() - 0.5);
+  return shuffled.slice(0, count);
+}
+
+module.exports = {
+  addTrackedTicker,
+  removeTrackedTicker,
+  getTrackedTickers,
+  getAllSnipers,
+  getSniperRotation
+};

449768df191338237d23889e2321777a8a953359
martymods <50667313+martymods@users.noreply.github.com>
2025-04-17 14:01:58 -0400

    Add files via upload

---
+++ economy/bettingStatsModel.js
@@ -0,0 +1,10 @@
+// economy/bettingStatsModel.js
+const mongoose = require('mongoose');
+
+const BettingStatsSchema = new mongoose.Schema({
+  userId: String,
+  guildId: String,
+  tokensWon: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('BettingStats', BettingStatsSchema);

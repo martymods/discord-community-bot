5408363334b735cc5bbd136ac192c314b0b6be1f
martymods <50667313+martymods@users.noreply.github.com>
2025-05-16 15:08:00 -0400

    Update registerReferral.js

--- utils/registerReferral.js
+++ utils/registerReferral.js
@@ -1,16 +1,27 @@
 // utils/registerReferral.js
 const Referral = require('../models/referralModel');
-const { Currency } = require('../economy/currency');
+const BettingStats = require('../economy/bettingStatsModel');
 
 async function registerReferral(codeUsed, newUserId, guildId) {
   const referral = await Referral.findOne({ code: codeUsed });
   if (!referral) return false;
 
   referral.referrals += 1;
-  referral.earned += 100; // Reward per referral
+  referral.earned += 25; // 25 DreamTokens earned
   await referral.save();
 
-  await Currency.addCash(referral.userId, guildId, 100);
+  // Update BettingStats token total
+  let stats = await BettingStats.findOne({ userId: referral.userId, guildId });
+  if (!stats) {
+    stats = await BettingStats.create({
+      userId: referral.userId,
+      guildId,
+      tokensWon: 25
+    });
+  } else {
+    stats.tokensWon += 25;
+    await stats.save();
+  }
 
   return true;
 }

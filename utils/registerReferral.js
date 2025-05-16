e7b341020c1a3e93b4c0ef9a655fae9d0fabf558
martymods <50667313+martymods@users.noreply.github.com>
2025-05-16 13:30:30 -0400

    Add files via upload

---
+++ utils/registerReferral.js
@@ -0,0 +1,18 @@
+// utils/registerReferral.js
+const Referral = require('../models/referralModel');
+const { Currency } = require('../currency');
+
+async function registerReferral(codeUsed, newUserId, guildId) {
+  const referral = await Referral.findOne({ code: codeUsed });
+  if (!referral) return false;
+
+  referral.referrals += 1;
+  referral.earned += 100; // Reward per referral
+  await referral.save();
+
+  await Currency.addCash(referral.userId, guildId, 100);
+
+  return true;
+}
+
+module.exports = { registerReferral };
